517546787372f6d6e16c9af97687b39b24b63b12
Sarra-Chouk <60300372@udst.edu.qa>
2024-11-05 16:00:21 +0300

    Add mongoDB connection setup

---
+++ persistence.js
@@ -0,0 +1,12 @@
+const { MongoClient } = require("mongodb")
+
+const uri = "mongodb+srv://60300372:INFS3201@infs3201.9arv1.mongodb.net/"
+const client = new MongoClient(uri)
+
+const db = "INFS3201-Project"
+
+async function connectToDatabase(collectionName) {
+    await client.connect()
+    console.log(`Connected to the database, accessing collection: ${collectionName}`)
+    return client.db(db).collection(collectionName)
+}
