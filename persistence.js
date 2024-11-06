b1e06f43b8226e51890577a864c5c2b439bdc1b1
RaneemAlhassan <ranemelhasan99@gmail.com>
2024-11-06 16:08:10 +0300

    enhancing database connection

--- persistence.js
+++ persistence.js
@@ -1,13 +1,17 @@
+const mongodb = require('mongodb');
 const { MongoClient } = require("mongodb")
 
-const uri = "mongodb+srv://60300372:INFS3201@infs3201.9arv1.mongodb.net/"
-const client = new MongoClient(uri)
+let client = undefined;
+let db = undefined;
+let users = undefined;
 
-const db = "INFS3201-Project"
 
-async function connectToDatabase(cName) {
-    await client.connect()
-    console.log(`Connected to the database, accessing collection: ${cName}`)
-    return client.db(db).collection(cTasks)
+async function connectDatabase() {
+    if (!client) {
+        client = new MongoClient("mongodb+srv://60300372:INFS3201@infs3201.9arv1.mongodb.net/"
+        );
+        await client.connect();
+        db = client.db("INFS3201-Project");
+        users = db.collection('users');
+    }
 }
-
